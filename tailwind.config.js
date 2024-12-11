107ee61738475bff5be710581b45c094c23819b6
OmaarAGR <omandres2505@gmail.com>
2024-12-11 11:06:04 -0500

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-  content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
-  ],
-  theme: {
-    theme: {
-      extend: {},
-    },
-  },
-  plugins: []
+    content: [
+        './node_modules/flowbite/**/*.js',
+        './_drafts/**/*.html',
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.md',
+        './*.html',
+    ],
+    darkMode: 'class',
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
